58538bdd2ef6e08371bee05f8d7479cf0bd44db7
clementdufour94 <clement.dufourl@gmail.com>
2024-05-16 15:58:22 +0200

    webmetric front

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import axios from 'axios';
 import os from 'os';
-import {Gauge, register} from 'prom-client';
+import { Gauge, register } from 'prom-client';
 dotenv.config();
 
 const app = express();
@@ -14,9 +14,9 @@ const serviceGauge = new Gauge({
   labelNames: ['service_url'],
 });
 
-const cpuGauge = new Gauge({name: 'cpu_usage', help: 'CPU usage'});
-const diskGauge = new Gauge({name: 'disk_usage', help: 'Disk usage'});
-const loadGauge = new Gauge({name: 'load_avg', help: 'Load average'});
+const cpuGauge = new Gauge({ name: 'cpu_usage', help: 'CPU usage' });
+const diskGauge = new Gauge({ name: 'disk_usage', help: 'Disk usage' });
+const loadGauge = new Gauge({ name: 'load_avg', help: 'Load average' });
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -30,12 +30,42 @@ app.get('/metrics', async (req, res) => {
   res.end(await register.metrics());
 });
 
+// Nouvelle route pour afficher les métriques de chaque serveur
+app.get('/services/metrics', async (req, res) => {
+  const services = ['http://localhost:3000', 'http://localhost:3005/api/pay']; // Remplacez ceci par vos services
+  const metrics = [];
+
+  for (const service of services) {
+    try {
+      const response = await axios.get(service);
+      metrics.push({
+        service: service,
+        status: response.status === 200 ? 'OK' : 'Error',
+        cpuUsage: os.loadavg()[0] / os.cpus().length,
+        diskUsage: 1 - os.freemem() / os.totalmem(),
+        loadAvg: os.loadavg()[0],
+      });
+    } catch (error) {
+      console.error(`Une erreur s'est produite lors de l'accès à ${service}:`, error);
+      metrics.push({
+        service: service,
+        status: 'Error',
+        cpuUsage: null,
+        diskUsage: null,
+        loadAvg: null,
+      });
+    }
+  }
+
+  res.json(metrics);
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
 setInterval(async () => {
-  const services = ['http://localhost:3001', 'http://example2.com']; // Remplacez ceci par vos services
+  const services = ['http://localhost:3000', 'http://localhost:3005/api/pay']; // Remplacez ceci par vos services
 
   for (const service of services) {
     try {
@@ -57,6 +87,4 @@ setInterval(async () => {
   // Load average
   const loadAvg = os.loadavg()[0];
   loadGauge.set(loadAvg);
-
-  // Network usage
 }, 5000);
